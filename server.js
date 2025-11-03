27ead1baee03116bdb6ab18c1dec78edb778bdf0
OWU131323241 <s1323241@cst.otsuma.ac.jp>
2025-11-03 07:42:59 +0000

    Fix root route to serve tilt-start-game.html

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const port = process.env.PORT || 3000; // ←重要！
+const port = process.env.PORT || 3000;
 
+// 静的ファイルを提供
 app.use(express.static(path.join(__dirname, "public")));
+
+// ★ルートアクセス時に tilt-start-game.html を返す
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "tilt-start-game.html"));
+});
 
 io.on("connection", (socket) => {
   console.log("client connected:", socket.id);
