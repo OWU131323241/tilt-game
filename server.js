23768a949e2be4cddb1da0b6e2b94502b064d182
OWU131323241 <s1323241@cst.otsuma.ac.jp>
2025-11-03 08:58:24 +0000

    Fix file path for Render and add debug logs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,10 +10,18 @@ const io = new Server(server);
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+const publicPath = path.join(__dirname, "public");
+const htmlPath = path.join(publicPath, "tilt-start-game.html");
 
+// ログでパス確認（Renderのログに出る）
+console.log("Public path:", publicPath);
+console.log("HTML file exists:", fs.existsSync(htmlPath));
+
+app.use(express.static(publicPath));
+
+// ルートアクセス時にHTML返す
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "tilt-start-game.html"));
+  res.sendFile(htmlPath);
 });
 
 io.on("connection", (socket) => {
